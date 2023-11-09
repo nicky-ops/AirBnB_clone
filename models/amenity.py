a15507e5500b879542044ac675a5fdf5bac2d01c
Nickson K Rop <nickyrutto@gmail.com>
2023-11-09 04:59:44 -0800

    amenity class

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""
+Module creates a class: Amenity
+"""
+
+
+class Amenity(BaseModel):
+    """Class inherits from the BaseModel class"""
+    name = ""
