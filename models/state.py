d8f5e018aca36f755034ae590d74f7bf0dd6c6f5
Nickson K Rop <nickyrutto@gmail.com>
2023-11-09 08:48:52 -0800

    state class

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""
+Module creates a class called State
+"""
+
+
+class State(BaseModel):
+    """
+    Class state inherits from the BaseModel
+    """
+    name = ""
