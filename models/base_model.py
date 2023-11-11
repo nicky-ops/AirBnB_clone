7adc70869feb83db7f424c906a32cf697200283b
Sarahkiss215 <skisiangani5@gmail.com>
2023-11-10 19:01:50 -0800

    Correct pycodestyle errors

--- models/base_model.py
+++ models/base_model.py
@@ -3,14 +3,16 @@ from datetime import datetime
 import uuid
 from models import storage
 """
-This class BaseModel takes care of the initializaion, serialization and deserialization of class instances.
+This class BaseModel takes care of the initializaion,
+serialization and deserialization of class instances.
 It defines all common attributes/methods for other classes
 """
 
 
 class BaseModel():
     """
-    Base class defines all common attributes/methods for other classes to inherit
+    Base class defines all common attributes/methods
+    for other classes to inherit
     """
     def __init__(self, *args, **kwargs):
         """
@@ -20,9 +22,11 @@ class BaseModel():
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime
+                    (kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"] = datetime.strptime
+                    (kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
@@ -35,7 +39,7 @@ class BaseModel():
         """
         defines string representation of an object
         """
-        return("{} {} {}".format(__class__.__name__,self.id, self.__dict__))
+        return("{} {} {}".format(__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """
@@ -46,7 +50,8 @@ class BaseModel():
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance
+        returns a dictionary containing all keys/values of
+        __dict__ of the instance
         """
         class_name = __class__.__name__
         obj_dict = self.__dict__.copy()
