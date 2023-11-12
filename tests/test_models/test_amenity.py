bac15bf2d009e22e8e4b64507379bdc7aed4341d
Sarahkiss215 <skisiangani5@gmail.com>
2023-11-12 09:43:05 -0800

    Correct pycodestyle errors

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -14,12 +14,14 @@ class TestAmenity(unittest.TestCase):
     test cases for the Amenity class
     """
     my_instance = Amenity()
+
     def test_instantiation(self):
         """
         test instantiation of Amenity class
         """
 
-        self.assertEqual(str(type(my_instance)), "<class 'models.amenity.Amenity'>")
+        self.assertEqual(str(type(my_instance)),
+                         "<class 'models.amenity.Amenity'>")
         self.assertIsInstance(my_instance, Amenity)
         self.assertTrue(issubclass(type(my_instance), BaseModel))
 
@@ -31,7 +33,6 @@ class TestAmenity(unittest.TestCase):
         for k, v in test_attributes.items():
             self.assertTrue(hasattr(my_instance, k))
             self.assertEqual(type(getattr(my_instance, k, None)), v)
-
 
 
 if __name__ == "__main__":
